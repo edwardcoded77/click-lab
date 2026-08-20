b4347832067e2f17c446544d0b31b7b311ec85d7
edwardcoded77 <eolagunju25@email.com>
2026-08-20 15:27:42 -0400

    the page lives in real files

---
+++ script.js
@@ -0,0 +1,102 @@
+let button1 = document.getElementById("b1");
+let button2 = document.getElementById("b2");
+let button3 = document.getElementById("b3");
+let button4 = document.getElementById("b4");
+let answer = document.getElementById("ans");
+let datetime  = document.getElementById("date-time");
+// let scoreInput = document.getElementById("score-1");
+
+
+
+button1.addEventListener("click", function () {
+    answer.textContent = "Incorrect!";
+});
+
+button2.addEventListener("click", function () {
+    answer.textContent = "Incorrect!";
+});
+
+button3.addEventListener("click", function () {
+    answer.textContent = "Correct!";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Score starts at zero by clicking the correct answer.
+// let score = 0;
+
+
+// // Tell if the answer has been answered
+// let answered = false;
+
+// // display datetime 
+// let d = new Date();
+// datetime.textContent = d.toLocaleString();
+
+
+
+// // Button 1
+// button1.addEventListener("click", function () {
+//     if (answered === false){
+//        answer.textContent = "Incorrect!";
+//        answered = true;
+//     }
+// });
+
+// // Button 2
+// button2.addEventListener("click", function () {
+//        if (answered === false){
+//        answer.textContent = "Incorrect!";
+//        answered = true;
+//     }
+// });
+
+
+// // Button 3
+// button3.addEventListener("click", function () {
+//     if (answered === false){ 
+//        answer.textContent = "Correct!";
+//        score = score + 1;
+//        scoreInput.textContent= "Your score is: " + score;
+//        answered = true;
+//        }
+// });
+
+// // Button 4
+// button4.addEventListener("click", function () {
+//        if (answered === false){
+//        answer.textContent = "Incorrect!";
+//        answered = true;
+//     }
+// });
