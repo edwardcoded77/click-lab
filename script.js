18355e59e6da3734fa781405a318010d097be821
edwardcoded77 <eolagunju25@email.com>
2026-08-20 16:55:33 -0400

    add answer checking and score, closes #1

--- script.js
+++ script.js
@@ -4,20 +4,49 @@ let button3 = document.getElementById("b3");
 let button4 = document.getElementById("b4");
 let answer = document.getElementById("ans");
 let datetime  = document.getElementById("date-time");
-// let scoreInput = document.getElementById("score-1");
+let scoreInput = document.getElementById("score-1");
+
+// display datetime 
+let d = new Date();
+datetime.textContent = d.toLocaleString();
 
 
+// Score starts at zero by clicking the correct answer.
+let score = 0;
 
+
+// Track whether the question was answered
+ let answered = false;
+
+// Button 1
 button1.addEventListener("click", function () {
-    answer.textContent = "Incorrect!";
+  if (answered === false) {
+      answer.textContent = "Incorrect!";
+      answered = true;
+    }
+
 });
 
+// Button 2
 button2.addEventListener("click", function () {
-    answer.textContent = "Incorrect!";
+   if (answered === false) {
+         answer.textContent = "Incorrect!";
+         answered = true;
+    }
 });
 
+// Button 3
 button3.addEventListener("click", function () {
-    answer.textContent = "Correct!";
+    if (answered === false) {
+       answer.textContent = "Correct!";
+       score = score + 1;
+       scoreInput.textContent = "Your score is: " + score; 
+        answered = true;
+
+        console.log("answered:", answered);
+        console.log("score:", score);
+    }
+    
 });
 
 
